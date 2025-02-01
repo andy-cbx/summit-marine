f2c4dd7861f7b235e28f42f7e7dc1a7da57b06e1
xbrownx <asbrownn@gmail.com>
2025-02-01 20:09:24 +1100

    init commit

--- src/config/paths.tsx
+++ src/config/paths.tsx
@@ -1,1 +1,2 @@
-export const paths = {	root: {		path: '',		getHref: () => '/',	},	home: {		path: '/',		getHref: () => '/',	},	/* PLOP_INJECT_PATH */} as const;
+export const paths = {	root: {		path: '',		getHref: () => '/',	},	home: {		path: '/',		getHref: () => '/',	},	/* PLOP_INJECT_PATH */
+	services: {		path: 'services',		getHref: () => '/services',	},} as const;

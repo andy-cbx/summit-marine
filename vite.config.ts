ef3f75efb7152048556ecd2132602e280cf59382
xbrownx <asbrownn@gmail.com>
2025-02-02 18:14:24 +1100

    init commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,1 @@
-import react from '@vitejs/plugin-react';import { defineConfig } from 'vite';import viteTsconfigPaths from 'vite-tsconfig-paths';// https://vitejs.dev/config/export default defineConfig({	plugins: [react(), viteTsconfigPaths()],	base: '/summit-marine',});
+import react from '@vitejs/plugin-react';import { defineConfig } from 'vite';import viteTsconfigPaths from 'vite-tsconfig-paths';// https://vitejs.dev/config/export default defineConfig({	plugins: [react(), viteTsconfigPaths()],	base: './',});

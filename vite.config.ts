38515d5d6f6f1ef56a1e1c74a1f6e0d044a26e7f
xbrownx <asbrownn@gmail.com>
2025-02-02 17:47:43 +1100

    init commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react(), viteTsconfigPaths()],
-})
+import react from '@vitejs/plugin-react';import { defineConfig } from 'vite';import viteTsconfigPaths from 'vite-tsconfig-paths';// https://vitejs.dev/config/export default defineConfig({	base: '.',	plugins: [react(), viteTsconfigPaths()],});

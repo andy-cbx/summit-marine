0c2de68c0086709fd555514d3686e4d6307cfbe7
andy-cbx <andy@cbxcreative.com>
2026-02-01 13:09:20 +1100

    fine tune

--- src/features/header/header.tsx
+++
@@ -1,1 +0,0 @@
-/* PLOP_INJECT_IMPORT */export const Header = () => {	return <header></header>;};
--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,1 @@
-import react from '@vitejs/plugin-react';import { defineConfig } from 'vite';import viteTsconfigPaths from 'vite-tsconfig-paths';// https://vitejs.dev/config/export default defineConfig({	plugins: [react(), viteTsconfigPaths()],	base: './',});
+import react from '@vitejs/plugin-react';import { defineConfig } from 'vite';import viteTsconfigPaths from 'vite-tsconfig-paths';import tailwindcss from '@tailwindcss/vite';// https://vitejs.dev/config/export default defineConfig({	plugins: [react(), viteTsconfigPaths(), tailwindcss()],	base: './',});
